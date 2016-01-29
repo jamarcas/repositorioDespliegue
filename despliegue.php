39e21dc8ec1fcc4266e523b356701663a17d77cc
Javier Martín <jamarcas1984@gmail.com>
2016-01-29 01:32:55 +0100

    Modificar archivo despliegue.php rama master

--- despliegue.php
+++ despliegue.php
@@ -1,9 +1,10 @@
 <?php
-	$file_sh = "ssh jamarcas@185.42.105.34 ./PUESTA_EN_PRODUCCION.sh";
+	$file_sh = "./PUESTA_EN_PRODUCCION.sh";
 
 	$payload = json_decode(file_get_contents('php://input'));
 
 	if ($payload->ref === 'refs/heads/master'){
-		shell_exec($file_sh);
+		//shell_exec($file_sh);
+		exec("./PUESTA_EN_PRODUCCION.sh {$payload->repository->url} {$payload->respository->name}", $output);
 	}
 
